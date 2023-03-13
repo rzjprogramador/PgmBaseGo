5f0b5fbf5033ef7497864a69c5e44bae98c26a4a
rzjprogramador <rzjprogramador@gmail.com>
2023-03-13 15:24:32 -0300

    feat: variaveis de ambinete golang com a lib godotenv

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/rzjprogramador/PgmBaseGo/tecnica"
+import (
+	"fmt"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+	// "github.com/rzjprogramador/PgmBaseGo/tecnica"
+)
+
 
 
 func main() {
-	tecnica.ExecuteTecnica()
+	// tecnica.ExecuteTecnica()
+
+	// Acessando variavelDeAmbiente definida no arquivo .env
+
+	// 1- carregar variavel - verificando se tem erro
+	err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+	// 2- usar variavelCarregada - passando a variavel como string
+	fmt.Println(os.Getenv("TEST"))
 }
