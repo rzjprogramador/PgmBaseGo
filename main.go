a3c25feb0230e1a5ce604847841be46228ac4da0
rzjprogramador <rzjprogramador@gmail.com>
2023-03-14 10:39:57 -0300

    refactor: organizando

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-	"strconv"
+// import (
+// 	// "github.com/rzjprogramador/PgmBaseGo/tecnica"
+// )
 
-	"github.com/rzjprogramador/PgmBaseGo/utils"
-	// "github.com/rzjprogramador/PgmBaseGo/tecnica"
-)
-
-func main() {
-	test := utils.UseLoadEnv("TEST")
-	nome := utils.UseLoadEnv("NOME")
-	idade, _ := strconv.Atoi(utils.UseLoadEnv("IDADE"))
-	tipoIdade := reflect.TypeOf(idade)
-	
-	fmt.Println(test, nome, idade)
-	fmt.Println(tipoIdade)
-}
+func main() {}
